f7ef569db9110b9abbff063afe291ebc1d1d4a25
Ahmed <ahmed.abdelaliem@gmail.com>
2018-11-29 09:11:13 +0000

    Update examples.php

--- php-examples/examples.php
+++ php-examples/examples.php
@@ -1,18 +1,18 @@
 <?php
 
 require('vendor/autoload.php');
-require('src/Snomen.php');
+require('src/Snomed.php');
 
-$snomen = new Snomen();
+$snomed = new Snomed();
 
 //Find/get a concept by a description SCTID (e.g. "679406011")
-echo $snomen->getConceptByString('Heart Attack');
+echo $snomed->getConceptByString('Heart Attack');
 
 //Find/get a concept by a concept SCTID (e.g. "109152007")
-echo $snomen->getConceptByDescription('679406011');
+echo $snomed->getConceptByDescription('679406011');
 
 //Find a concept by a string (e.g. "heart") but only in the Procedures semantic tag
-echo $snomen->getConceptBySCTID('109152007');
+echo $snomed->getConceptBySCTID('109152007');
 
 //Find a concept by a string (e.g. "heart attack")
-echo $snomen->getConceptByStringInProceduresSemanticTag('heart');
+echo $snomed->getConceptByStringInProceduresSemanticTag('heart');
